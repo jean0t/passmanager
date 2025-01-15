14c2d220c1af10213bafe84e63708e01e7750c2b
jean0t <joaomouryf@hotmail.com>
2025-01-14 22:52:44 -0300

    tests directory updated

---
+++ tests/test_random_password.py
@@ -0,0 +1,56 @@
+import pytest
+from random import choice
+from src.random_password import Password
+
+@pytest.fixture
+def passwd():
+    return Password()
+
+
+@pytest.fixture
+def length():
+    return choice(range(0, 100))
+
+
+def test_shuffle(passwd):
+    a = "random string"
+    assert passwd._secure_shuffle(list(a)) != passwd._secure_shuffle(list(a))
+
+@pytest.mark.parametrize("lengths", range(101))
+def test_password_generation_with_different_lengths(passwd, lengths):
+    password = passwd.Random_password(length=lengths, uppercase=True,
+                lowercase=True, digits=True, special=True)
+
+    if lengths == 0:
+        assert len(password) == 0
+        assert password == ""
+
+    else:
+        assert len(password) == lengths
+
+
+def test_password_characters(passwd, length):
+
+    password1 = passwd.Random_password(length=length, uppercase=True)
+    assert password1.isupper()
+
+    password2 = passwd.Random_password(length=length, lowercase=True)
+    assert password2.islower()
+
+    password3 = passwd.Random_password(length=length, digits=True)
+    assert password3.isdigit()
+
+    password4 = passwd.Random_password(length=length, special=True)
+    assert all(char in passwd.special for char in password4)
+
+
+
+def test_randomness_for_password(passwd, length):
+    
+    password1 = passwd.Random_password(length, uppercase=True,
+                    lowercase=True, digits=True, special=True)
+
+    password2 = passwd.Random_password(length, uppercase=True,
+                    lowercase=True, digits=True, special=True)
+
+    assert password1 != password2
