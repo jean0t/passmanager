392852fc3b179c134a657d4b4a02bc822a77a477
jean0t <joaomouryf@hotmail.com>
2025-01-17 00:38:47 -0300

    modified main and message

--- src/message.py
+++ src/message.py
@@ -12,3 +12,10 @@ class Message:
 	@staticmethod
 	def Info(message: str) -> None:
 		print(termcolor.colored(message, "light_yellow"))
+
+    @staticmethod
+    def Query_result(id: int, name: str, password: str, comments: str):
+        print(termcolor.colored("id:\t", "light_yellow"), termcolor.colored(f"{id}", "light_green"))
+        print(termcolor.colored("name:\t", "light_yellow"), termcolor.colored(f"{name}", "light_green"))
+        print(termcolor.colored("password:\t", "light_yellow"), termcolor.colored(f"{password}", "light_green"))
+        print(termcolor.colored("comments:\n\t", "light_yellow"), termcolor.colored(f"{comments}", "light_green"))
