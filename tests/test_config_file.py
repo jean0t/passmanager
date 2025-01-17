77bfc62f04f34d4844380837153864fc0f275a55
jean0t <joaomouryf@hotmail.com>
2025-01-17 00:37:16 -0300

    testing configs

---
+++ tests/test_config_file.py
@@ -0,0 +1,22 @@
+import sys
+sys.dont_write_bytecode = True
+
+from src.config import Config
+from pathlib import Path
+
+def test_app_path():
+    path = Path().home() / ".passmng"
+
+    assert path == Config.app_path
+
+def test_database_path():
+    path = Path().home() / ".passmng" / "database.db"
+
+    assert path == Config.database_path
+
+def test_if_directory_is_created():
+    path = Path().home() / "test_creation_of_directory"
+    Config.configure_environment(path)
+    
+    assert path.exists()
+    path.rmdir()
