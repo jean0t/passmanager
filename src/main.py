e602c0a2df1461ac233e771c1dae2e1436021293
jean0t <joaomouryf@hotmail.com>
2025-01-18 01:13:16 -0300

    feat: add, remove, query functions are created

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from os import system
 import sys
 from time import sleep
 
+
 class Passmanager():
     def __init__(self):
         Config.configure_environment(Config.app_path)
@@ -19,7 +20,7 @@ class Passmanager():
         try:
             self.database = Password(password=password)
             Message.Success("User authenticated!")
-            sleep(2)
+            sleep(1.5)
         except:
             Message.Failure("User couldn't be authenticated!")
             sleep(2)
@@ -27,8 +28,60 @@ class Passmanager():
 
 
     def Menu(self):
-        pass
+        Message.Info("Use the command 'help' to see the commands")
+        while True:
+            prompt = input("> ").strip().lowercase()
 
+            match prompt:
+                case "help":
+                    Message.Info("generate -> generates a random password")
+                    Message.Info("query_all -> see all password")
+                    Message.Info("query_one [ID] -> see a specific password")
+                    Message.Info("add -> add a new password to database")
+                    Message.Info("update [ID] -> update the data")
+                    Message.Info("remove [ID] -> remove a specific data (Be careful!)")
+                    Message.Info("clear -> clear the screen")
+                    Message.Info("exit -> leaves the program")
+                
+                case "generate":
+                    pass
+
+                case "query_one":
+                    if len(prompt.split(" ")) == 2:
+                        self.database.Query_one(int(prompt.split(" ")[1]))
+                    else:
+                        Message.Failure("Please provide an ID.")
+
+                case "query_all":
+                    self.database.Query_all()
+
+                case "remove":
+                    if len(prompt.split(" ")) == 2:
+                        if input("Are you sure? (y/n) ").strip().lowercase() == "y":
+                            self.database.Remove(int(prompt.split(" ")[1]))
+                        else:
+                            Message.Info("Operation aborted.")
+                    else:
+                        Message.Failure("Please provide an ID.")
+
+                case "update":
+                    if len(prompt.split(" ")) == 2:
+                        self.database.Update(id= int(prompt.split(" ")[1]), username= input("username: ").strip(), password= input("password: ").strip(), comments= input("comments: ").strip())
+                        Message.Success("Updated Successfuly.")
+                    else:
+                        Message.Failure("Please provide an ID.")
+
+                case "add":
+                    self.database.Add(username= input("username: ").strip(), password= input("password: ").strip(), comments= input("comments: ").strip())
+                    Message.Success("Data added to database.")
+
+                case "clear":
+                    system("clear")
+
+
+                case "exit":
+                    self.database.Close()
+                    exit(0)
 
     def Exit(self):
         self.database.Close()
